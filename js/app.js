e0ebf9a35956f5cd84871749afd7ca6a65473ad7
amdas72@gmail.com <Moradeyo123>
2019-10-10 14:26:35 +0300

    Changes made to directory structure

--- js/app.js
+++ js/app.js
@@ -25,8 +25,8 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
     //2. Display result
     document.getElementById("dice-1").style.display = "block";
     document.getElementById("dice-2").style.display = "block";
-    document.getElementById("dice-1").src = "/img/dice-" + dice1 + ".png";
-    document.getElementById("dice-2").src = "/img/dice-" + dice2 + ".png";
+    document.getElementById("dice-1").src = "img/dice-" + dice1 + ".png";
+    document.getElementById("dice-2").src = "img/dice-" + dice2 + ".png";
 
     //3. Update the round score IF the rolled number was not a 1
     if (dice1 !== 1 && dice2 !== 1) {
